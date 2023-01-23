05c3a14e7bd945fd5024a9ff37df31d503a3b790
简律纯 <Hsiangnianian@outlook.com>
2023-01-23 22:03:26 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,9 +23,7 @@ const config = {
   projectName: 'retro', // Usually your repo name.
   
   
-  // plugins
-  themes: ['@docusaurus/theme-live-codeblock'],
-  
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
