5f77bbb0dab40d7bf7f698bfc6a0895b1bff1f7e
简律纯 <Hsiangnianian@outlook.com>
2023-01-23 19:29:26 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/retrofor/retrofor.github.io/blob/master/docs/',
+            'https://github.com/retrofor/retrofor.github.io/blob/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/retrofor/retrofor.github.io/blob/master/blog/',
+            'https://github.com/retrofor/retrofor.github.io/blob/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
