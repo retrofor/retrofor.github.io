c3562c37404539042c4fb209b1ceed3ee8ca6c68
简律纯 <Hsiangnianian@outlook.com>
2023-02-02 01:50:31 +0800

    ✨新增Anouncement

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -141,6 +141,14 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+    announcementBar: {
+      id: 'support_us',
+      content:
+        'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+      backgroundColor: '#fafbfc',
+      textColor: '#091E42',
+      isCloseable: false,
+    },
 };
 
 module.exports = config;
